574efc5cda4e5d9430c433f8275a648e65dd906b
ManupaDev <manupadevsuite@gmail.com>
2023-05-08 19:27:25 +0530

    init

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from "express";
+import { Prisma, PrismaClient } from "@prisma/client";
+const cors = require("cors");
+export const prisma = new PrismaClient();
+
+const app = express();
+
+//! Global Midlleware
+app.use(cors());
+app.use(express.json());
+
+//! Mount Routers
+
+const port = process.env.PORT || 8000;
+
+const server = app.listen(port, () =>
+  console.log(`
+🚀 Server ready at: http://localhost:${port}`)
+);
