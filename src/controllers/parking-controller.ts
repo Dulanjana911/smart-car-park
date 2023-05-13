4baa4fee5a1257e8f2271e05ab21ef52f10721f8
ManupaDev <manupadevsuite@gmail.com>
2023-05-13 18:16:14 +0530

    feat: added directive to include user in the return query

--- src/controllers/parking-controller.ts
+++ src/controllers/parking-controller.ts
@@ -2,7 +2,11 @@ import { Request, Response } from "express";
 import { prisma } from "../index";
 
 export const getAllParkings = async (req: Request, res: Response) => {
-  const parkings = await prisma.parking.findMany();
+  const parkings = await prisma.parking.findMany({
+    include: {
+      user:true,
+    },
+  });
 
   res.status(200).json({
     status: "success",
