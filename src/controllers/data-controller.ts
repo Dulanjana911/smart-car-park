642ed4e50bcb7f75ffbc4d75cec5f5d1abfd886c
ManupaDev <manupadevsuite@gmail.com>
2023-05-13 16:45:26 +0530

    feat: readerId parsing

--- src/controllers/data-controller.ts
+++ src/controllers/data-controller.ts
@@ -5,7 +5,9 @@ export const handleData = async (req: Request, res: Response) => {
   
   const rfidRaw = req.query.rfid as string;
   const rfid = rfidRaw.replace(/-/g, ""); 
-  console.log(rfid);
+  
+  const readerId = req.query.reader as string; 
+  console.log(rfid,readerId);
   
 
 
