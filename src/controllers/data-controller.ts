8009a65e5ffd20ed1c394e1e8df0b7a5e0553f94
ManupaDev <manupadevsuite@gmail.com>
2023-05-27 20:15:42 +0530

    feat: added error handling

--- src/controllers/data-controller.ts
+++ src/controllers/data-controller.ts
@@ -2,25 +2,31 @@ import { Request, Response } from "express";
 import { prisma } from "../index";
 
 export const handleData = async (req: Request, res: Response) => {
-  
-  const rfidRaw = req.query.rfid as string;
-  const rfid = rfidRaw.replace(/-/g, ""); 
+  try {
+    const rfidRaw = req.query.rfid as string;
+    const rfid = rfidRaw.replace(/-/g, "");
 
-  const readerId = req.query.reader as string; 
-  const action = readerId === "0" ? "entry" : "exit"  
+    const readerId = req.query.reader as string;
+    const action = readerId === "0" ? "entry" : "exit";
 
-  console.log(rfid,action);
-  
-  const newParking = await prisma.parking.create({
-    data: {      
-      rfid: rfid,
-      time:new Date(),
-      action:action
-    },
-  });
+    console.log(rfid, action);
 
-  res.status(200).json({
-    status: "success",
-    data:newParking
-  });
+    const newParking = await prisma.parking.create({
+      data: {
+        rfid: rfid,
+        time: new Date(),
+        action: action,
+      },
+    });
+
+    res.status(200).json({
+      status: "success",
+      data: newParking,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "fail",
+      data: {},
+    });
+  }
 };
